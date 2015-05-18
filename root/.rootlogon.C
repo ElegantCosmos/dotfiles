dca23cb2601aecf7aa03179f2f6ae1ead87d6362
mich <michinar@hawaii.edu>
2015-05-17 17:45:19 -1000

    root/.rootlogon.C corrected to use files in ~/.dotfiles/

--- root/.rootlogon.C
+++ root/.rootlogon.C
@@ -1,5 +1,5 @@
 {
-	std::string styleFile_str = "~/data/personal/root/style.root";
+	std::string styleFile_str = "~/.dotfiles/root/style.root";
 	TFile *styleFile = new TFile(styleFile_str.c_str(), "read");
 	TStyle *style = NULL;
 	styleFile->GetObject("style", style);
