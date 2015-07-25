ed56646ed1bafaf84e6b234abe7c7fb93a553b1b
mich <michinar@hawaii.edu>
2015-07-25 07:14:15 -1000

    20150724a

--- root/.rootlogon.C
+++ root/.rootlogon.C
@@ -1,8 +1,8 @@
 {
-	std::string styleFile_str = "~/.dotfiles/root/style.root";
-	TFile *styleFile = new TFile(styleFile_str.c_str(), "read");
-	TStyle *style = NULL;
-	styleFile->GetObject("style", style);
-	style->cd();
-	gROOT->ForceStyle(1);
+	//std::string styleFile_str = "~/.dotfiles/root/style.root";
+	//TFile *styleFile = new TFile(styleFile_str.c_str(), "read");
+	//TStyle *style = NULL;
+	//styleFile->GetObject("style", style);
+	//style->cd();
+	//gROOT->ForceStyle(1);
 }
