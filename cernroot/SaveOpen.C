bb76b3e6fdbd27abedabadd699fa72ae1853f8c1
Michinari Sakai <michsakai@ucla.edu>
2017-11-24 15:20:48 +0100

    Improve SaveOpen ROOT script to save macro for plot too.

--- cernroot/SaveOpen.C
+++ cernroot/SaveOpen.C
@@ -1,11 +1,26 @@
-void SaveOpen(const char* file = "test.pdf", int nFiles = 1) // Draw and copy quickly with one function.
+void SaveOpen(const std::string file = "plot.pdf", int nFiles = 1) // Draw and copy quickly with one function.
 {
-  assert(strcmp(file, "") != 0); // filename must not be empty
+  assert(!file.empty()); // filename must not be empty
   if (gPad == NULL) {
 	  std::cerr << "No gPad object found for plot." << std::endl;
 	  return;
   }
-  gPad->SaveAs(file);
+  std::string file_pdf = file;
+  if (file_pdf.find(".pdf") == std::string::npos) {
+    file_pdf += ".pdf";
+  }
+  gPad->SaveAs(file_pdf.c_str());
+
+  std::string file_C = file;
+  if (file_C.find(".pdf") != std::string::npos) {
+    size_t found = file_C.find(".pdf");
+    file_C = file_C.substr(0, file_C.length() - std::string(".pdf").length());
+    file_C += ".C";
+  } else {
+    file_C += ".C";
+  }
+  gPad->SaveAs(file_C.c_str());
+
 
   assert(nFiles > 0);
   if (nFiles == 1) {
