fccade500f3ebc45839e285c8ca2ce00e8e6779a
michsakai <michsakai@gmail.com>
2017-12-08 12:12:14 -0800

    Edit rootlogon.C to correctly search for existence of script.

--- cernroot/.rootlogon.C
+++ cernroot/.rootlogon.C
@@ -1,8 +1,8 @@
 {
-	if (gSystem->AccessPathName("~/.rootlogon_local.C") == kTRUE) { // execute if file exists
+	if (gSystem->AccessPathName("~/.rootlogon_local.C") == kFALSE) { // execute if file exists
 		gROOT->ProcessLine(".x ~/.rootlogon_local.C");
 	}
-	if (gSystem->AccessPathName("~/.dotfiles/cernroot/SaveOpen.C") == kTRUE) { // load if file exists
+	if (gSystem->AccessPathName("~/.dotfiles/cernroot/SaveOpen.C") == kFALSE) { // load if file exists
 		gROOT->ProcessLine(".L ~/.dotfiles/cernroot/SaveOpen.C");
 	}
 	//if (gSystem->AccessPathName("~/.dotfiles/cernroot/rootlogon_style.C") == kTRUE) {
