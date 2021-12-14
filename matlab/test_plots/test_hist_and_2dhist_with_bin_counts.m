8b8648aedc49de8fb4973153636deda1b45f9044
Sakai, Mich <Mich.Sakai@jhuapl.edu>
2021-12-14 12:36:57 -0500

    Fix example MatLAB code for setting 2-D histogram log colorbar scale with colorbar limits.

--- matlab/test_plots/test_hist_and_2dhist_with_bin_counts.m
+++ matlab/test_plots/test_hist_and_2dhist_with_bin_counts.m
@@ -7,9 +7,10 @@ figure(3);
 bin_counts = [0, 1, 3, 4, 3, 4, 7, 3, 4, 10];
 bin_edges = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 histogram('BinEdges', bin_edges, 'BinCounts', bin_counts, 'DisplayStyle', 'stairs');
-%xlim([0, 10]);
-%ylim([0.5, 3.5]);
-%set(gca, 'XScale', 'log');
+ax = gca;
+ax.XScale = 'log';
+ax.YScale = 'log';
+ax.YLim = [1, 12]
 title('Test histogram');
 xlabel('X-axis');
 ylabel('Counts per bin');
@@ -20,27 +21,24 @@ print('test_hist', '-dpdf', '-painters')
 %%% 2-D histogram:
 figure(4);
 
-bin_counts = [1e-5, 1e-6, 1e-7; 1e-8, 1e-9, 1e-9; 1e-9, 1e-9, 1e-9];
+bin_counts = [0, 0.01, 0.1; 0, 1, 10; 0, 1, 10];
 x_edges = [1, 2, 3, 4];
 y_edges = [4, 5, 6, 7];
 histogram2('XBinEdges', x_edges, 'YBinEdges', y_edges, 'BinCounts', bin_counts, 'DisplayStyle', 'tile', 'LineStyle', 'none');
-%xlim([0, 4]);
 grid('off'); % don't show grid
 title('Test 2-D histogram');
 xlabel('$\log_{10}{10} E_{gqyp} 10^{10}/\sqrt{2}$ [cm]');
 ylabel('$\log_{10}{10} E_{gqyp} 10^{10}/\sqrt{2}$ [cm]');
 
-% Axis
-%ax = gca;
-%ax.YAxis.Exponent = 1;
-
 % Color bar
 cb = colorbar();
-%cb.Ruler.Limits = [0.5, 1e7];
+cb.Label.Interpreter = 'latex';
 cb.Label.String = '$\log_{10}{10} E_{gqyp} 10^{10}/\sqrt{2}$ [cm]';
-cb.Ruler.Scale = 'log';
-% cb.Ruler.Limits = [0.5, 1000]
+ax = gca; % axis parameters need to be set before format_colorbar() for some reason
+%ax.YAxis.Exponent = 1;
+ax.ColorScale = 'log';
+ax = gca;
+ax.CLim = ([1e-2 20]);
 format_colorbar(cb);
-%cb.Ruler
 
 print('test_hist2', '-dpdf', '-painters') % -painters needed for hi-res
