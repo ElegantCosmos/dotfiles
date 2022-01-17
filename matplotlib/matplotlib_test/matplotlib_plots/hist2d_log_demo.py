0c6639c055122e23a58f2e9cc707468651f08045
Sakai, Mich <Mich.Sakai@jhuapl.edu>
2022-01-17 07:32:58 -0500

    Update matplotlib scripts.

--- matplotlib/matplotlib_test/matplotlib_plots/hist2d_log_demo.py
+++ matplotlib/matplotlib_test/matplotlib_plots/hist2d_log_demo.py
@@ -11,20 +11,21 @@ def colorbar(mappable):
     return fig.colorbar(mappable, cax=cax)
 
 # normal distribution center at x=0 and y=0
-bin_edges_x = (0, 1, 2, 3)
-bin_edges_y = (10, 11, 12, 13)
-xs = [0.5, 1.5, 2.5]
-ys = [11.5, 12.5, 12.5]
+bin_edges_x = (0, 100, 200, 300)
+bin_edges_y = (100, 110, 120, 130)
+xs = [50, 150, 250]
+ys = [115, 125, 125]
 ws = [0.1, 1e-7, 100]
 
 fig, ax = plt.subplots()
-hist, xedges, yedges, image = ax.hist2d(xs, ys, bins=(bin_edges_x, bin_edges_y), weights=ws, norm=LogNorm()); label_pad_cb = -31.5
+hist, _, _, image = ax.hist2d(xs, ys, bins=(bin_edges_x, bin_edges_y), weights=ws, norm=LogNorm()); label_pad_cb = -32.5
 #hist, _, _, image = ax.hist2d(xs, ys, bins=(bin_edges_x, bin_edges_y), weights=ws, vmin=0, cmin=0.01); label_pad_cb = -28.5
 plt.title("Sample title")
 ax.set_xlabel(r"$\log{10}_{10}$ $E_{gqyp}$ $10^{10}$/$\sqrt{2}$ [cm]", labelpad=None)
 ax.set_ylabel(r"$\log{10}_{10}$ $E_{gqyp}$ $10^{10}$/$\sqrt{2}$ [cm]", labelpad=None)
 
-cax = fig.add_axes([0.912, 0.172, 0.01, 0.74])
+cax = fig.add_axes([0.912, 0.172, 0.010, 0.740]) # 90 mm plot width
+#cax = fig.add_axes([(190 - 90*(1 - 0.912))/190, 0.172, 0.010*(90/190), 0.740]) # 190 mm plot width
 cbar = fig.colorbar(image, cax=cax, orientation='vertical')
 cax.set_ylabel(r"$E_{gqyp}$ $\log{10}_{10}$ $10^{10}$ $\sqrt{2}$ [cm]",
 		labelpad=label_pad_cb)
