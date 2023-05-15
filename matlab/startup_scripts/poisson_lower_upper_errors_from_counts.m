db37d63400228ed8c542bda42f62fb6238974f28
sakaim1 <Mich.Sakai@jhuapl.edu>
2023-05-15 07:12:37 -0400

    Moved Poisson error scripts to mcnp-tools repository.

--- matlab/startup_scripts/poisson_lower_upper_errors_from_counts.m
+++
@@ -1,9 +0,0 @@
-%%% Function to get Poisson lower and upper vertical error bar lengths from number of counts.
-function [lower, upper] = poisson_lower_upper_errors_from_counts(counts, confidence_level)
-	assert(confidence_level >= 0 & confidence_level <= 1);
-	assert(all(counts >= 0));
-
-	alpha = 0.5*(1 - confidence_level);
-	lower = abs(counts - 0.5*chi2inv(alpha, 2*counts)); % magnitude(s) of lower uncertainty
-	upper = abs(0.5*chi2inv(1 - alpha, 2*(counts + 1)) - counts); % magnitude(s) of upper uncertainty
-end
