5d8772a8080cd160a4573c503d1593ab371f6afd
sakaim1 <Mich.Sakai@jhuapl.edu>
2023-07-13 16:52:41 -0400

    Update Cern-ROOT setup script.

--- cernroot/cernroot_test/cernroot_plots/setup.sh
+++ cernroot/cernroot_test/cernroot_plots/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 ### Cern-ROOT
-if [[ "$HOSTNAME" == "vmUbuntuG4" ]]; then
-  source ${HOME}/.opt/cernroot/root-6.22.02/bin/thisroot.sh
-elif [[ "$HOSTNAME" == "kahr" ]]; then
-  source ${HOME}/.opt/cernroot/root-6.22.06/bin/thisroot.sh
+if [[ "$HOSTNAME" == "amds-a3g-ld01" ]]; then
+  source ${HOME}/.opt/cernroot/root_v6.24.08/bin/thisroot.sh
 fi
